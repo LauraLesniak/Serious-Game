516f394fd69b3ea365c424679b9cba30d3ff9df9
oskarupa <o.skarupa@student.tue.nl>
2025-01-16 13:30:28 +0100

    dragging in airlock game

--- Serious Game/Assets/RobotControlGame/dragItem.cs
+++ Serious Game/Assets/RobotControlGame/dragItem.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using TMPro; // Import TextMeshPro namespace
+using TMPro;
+using UnityEditor.Timeline; // Import TextMeshPro namespace
 
 public class DragItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
@@ -24,7 +25,22 @@ public class DragItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
 
     public void OnEndDrag(PointerEventData eventData) {
         Debug.Log("End Drag");
+
+        //detect inbetween which objects in the list the item is dropped and set the item's sibling index
+        int newSiblingIndex = parentAfterDrag.childCount;
+        for (int i = 0; i < parentAfterDrag.childCount; i++) {
+            if (transform.position.y > parentAfterDrag.GetChild(i).position.y) {
+                newSiblingIndex = i;
+                // if (transform.GetSiblingIndex() < newSiblingIndex) {
+                //     newSiblingIndex--;
+                // }
+                break;
+            }
+        }
         transform.SetParent(parentAfterDrag);
+        transform.SetSiblingIndex(newSiblingIndex);
+
         text.raycastTarget = true; // Corrected property name
     }
+
 }
