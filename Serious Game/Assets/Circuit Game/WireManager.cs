be1ba885ca03322c0ba9fff66365856a2a0453c3
oskarupa <o.skarupa@student.tue.nl>
2025-01-27 18:50:48 +0100

    circuit game dragging fixes

--- Serious Game/Assets/Circuit Game/WireManager.cs
+++ Serious Game/Assets/Circuit Game/WireManager.cs
@@ -23,8 +23,10 @@ public class WireManager : MonoBehaviour
 
         if (Input.GetMouseButtonUp(0) && isDrawing) // On mouse release
         {
-            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-
+            int inputIOLayer = LayerMask.NameToLayer("Gate IO");
+            int layerMask = 1 << inputIOLayer;
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, Mathf.Infinity, layerMask);
+            Debug.Log(hit.collider);
             if (hit.collider != null && currentWire != null)
             {
                 if (hit.collider.CompareTag("InputPort"))
@@ -38,6 +40,13 @@ public class WireManager : MonoBehaviour
                     currentWire = null;
                     isDrawing = false;
                 }
+            }
+            
+            if (hit.collider == null)
+            {
+                Destroy(currentWire.gameObject);
+                currentWire = null;
+                isDrawing = false;
             }
         }
 
